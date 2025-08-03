0cefdc906dce2c5450924825b9de437f6c16b39c
pratiksha950 <salunkepratiksha950@gmail.com>
2025-08-03 19:45:39 +0530

    promse chaining

--- dom.js
+++ dom.js
@@ -282,6 +282,29 @@
 // });
 
 // "*promises*"
+// function saveDT(Data){
+//   return new Promise((success,failure)=>{
+//     let speedLimit=Math.floor(Math.random()*10)+1;
+//     if(speedLimit>4){
+//      success("data was saved");
+//     }else{
+//       failure("data was not saved");
+//     }
+//   })
+// }
+// saveDT("pratiksha");
+
+// **promise Method then and catch**
+// saveDT("pratiksha")
+// .then(()=>{
+//   console.log("data saved,success")
+// })
+// .catch(()=>{
+//   console.log("data was not saved!!! failure")
+// });
+
+// **promse chaining**
+
 function saveDT(Data){
   return new Promise((success,failure)=>{
     let speedLimit=Math.floor(Math.random()*10)+1;
@@ -292,13 +315,19 @@ function saveDT(Data){
     }
   })
 }
-// saveDT("pratiksha");
-
-// **promise Method then and catch**
+saveDT("pratiksha");
 saveDT("pratiksha")
 .then(()=>{
-  console.log("data saved,success")
+console.log("data1 saved,success");
+return saveDT("helloworld")
+})
+.then(()=>{
+  console.log("data2 saved,success");
+  return saveDT="Salunke";
+})
+.then(()=>{
+  console.log("data3 saved,success");
 })
 .catch(()=>{
   console.log("data was not saved!!! failure")
-});
+});  
