7c80110cb0a1971a9014b9103605803027425f3c
pratiksha950 <salunkepratiksha950@gmail.com>
2025-08-03 18:08:40 +0530

    callback hell

--- dom.js
+++ dom.js
@@ -231,3 +231,28 @@
 // console.log(ans);
 // }
 // three();
+
+// callback hell
+// h1=document.querySelector("h1");
+// function changeColor(color,delay,nextColorChange){
+//   setTimeout(()=>{
+//     h1.style.color=color;
+//     if(nextColorChange) nextColorChange();
+//   },delay)
+// }
+
+// changeColor("red",1000,()=>{
+// changeColor("blue",1000,()=>{
+//   changeColor("orange",1000);
+// });
+// });
+
+// changeColor("green",4000);
+// setTimeout(()=>{
+//   h1.style.color="orange";
+// },2000);
+
+// setTimeout(()=>{
+//   h1.style.color="red";
+// },1000);
+
